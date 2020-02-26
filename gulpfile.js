dd00fee93f122eb50052649a8c9fff203c8a6077
Zach <dev@zachakbar.com>
2020-02-26 11:35:06 -0700

    cleaned up core boilerplate items

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,34 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var bourbon = require('bourbon').includePaths;
-var connect = require("gulp-connect");
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
-var log = require('fancy-log');
+var notify = require('gulp-notify');
+var pump	= require('pump');
 
 // PATH objects
 var paths = {
-	js: ['_source/js/*.js'],
-	scss: ['./_source/scss/**/*.scss'],
-	inc: [bourbon, 'node_modules/susy/sass', 'node_modules/breakpoint-sass/stylesheets']
+	js: ['_src/js/_functions.js','_src/js/_plugins.js','_src/js/scripts.js'],
+	scss: ['./_src/scss/**/*.scss'],
+	inc: [bourbon, 'node_modules/breakpoint-sass/stylesheets']
 };
 
 // Minify JS
 gulp.task('js', function(e) {
 	pump([
 			gulp.src(paths.js),
-			concat('prop-scripts.js'),
+			concat('scripts.js'),
 			uglify(),
 			gulp.dest('assets/js/')
-		],
-		e
-	)
-	.on('end',function(){
-		log('**************************************');
-		log('************ JS COMPLETED ************');
-		log('**************************************');
-	});
+		],e)
+		.pipe(notify({ message: 'JS complete!' }));
 });
 
 // Compile SASS files
 gulp.task('sass', function() {
 	gulp.src(paths.scss)
-		.pipe(sourcemaps.init())
 		.pipe(sass({
 			outputStyle: 'compressed',
 			includePaths: paths.inc
@@ -47,25 +38,14 @@ gulp.task('sass', function() {
 			cascade: false
 		}))
 		.pipe(gulp.dest("assets/css/"))
-		.on('end',function(){
-			log('***************************************');
-			log('************ CSS COMPLETED ************');
-			log('***************************************');
-		});
-});
-
-// Set up localhost server
-gulp.task('connect', function() {
-  connect.server({
-    port: 8000
-  });
+		.pipe(notify({ message: 'CSS complete!' }));
 });
 
 // Watch
 gulp.task("watch", function() {
-	watch('_source/scss/**/*.scss', function() { gulp.start('sass'); });
-	watch('_source/js/**/*.js', function() { gulp.start('js'); });
+	watch('_src/scss/**/*.scss', function() { gulp.start('sass'); });
+	watch('_src/js/**/*.js', function() { gulp.start('js'); });
 });
 
 // Compile all gulp tasks
-gulp.task('default', ['connect', 'js', 'sass', 'watch']);
+gulp.task('default', ['js', 'sass', 'watch']);
